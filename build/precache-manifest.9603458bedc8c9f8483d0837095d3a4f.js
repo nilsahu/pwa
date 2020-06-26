80806c2a147f40deb59e8ae8ffb35ec20039c172
nilesh <nilesh2007015@gmail.com>
2020-06-26 11:33:45 +0530

    master code

---
+++ build/precache-manifest.9603458bedc8c9f8483d0837095d3a4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0555af75917c602a26d131e1937890",
+    "url": "/pwa-react/index.html"
+  },
+  {
+    "revision": "1f5be1c4824ddb9bee58",
+    "url": "/pwa-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24ad31aab1fcf92d711c",
+    "url": "/pwa-react/static/css/main.919e4cb9.chunk.css"
+  },
+  {
+    "revision": "1f5be1c4824ddb9bee58",
+    "url": "/pwa-react/static/js/2.f44fed71.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/pwa-react/static/js/2.f44fed71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ad31aab1fcf92d711c",
+    "url": "/pwa-react/static/js/main.4ecb1c01.chunk.js"
+  },
+  {
+    "revision": "5a8cb403ebfb5d879736",
+    "url": "/pwa-react/static/js/runtime-main.25aa9519.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
